6c032ac03fda416216a9a3bffb720e928076c929
Axel Bjelke <it@abjelke.no>
2026-01-11 20:49:40 +0100

    added Synology Drive

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -18,6 +18,14 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Synology Drive from COPR
+dnf5 -y copr enable emixampp/synology-drive
+dnf5 -y install synology-drive-noextra
+dnf5 -y copr disable emixampp/synology-drive
+
+# Removes those cached packages and metadata, which is standard practice in image builds.
+dnf5 clean all
 
 #### Example for enabling a System Unit File
 
